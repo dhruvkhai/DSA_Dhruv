a6321982da51d1dd71875e0fcf5e36815db56c69
Dhruv Khaitan <129095159+dhruvkhai@users.noreply.github.com>
2025-07-05 12:09:11 +0530

    Time: 0 ms (100%), Space: 45.7 MB (97.15%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int[] result = new int[2];
-        if(nums.length == 0) {
-        return new int[]{-1, -1};
-        }
-
-        int i = 0, start = -1, end = -1;
-        while(i<nums.length){
-            if(nums[i] == target){
-                start = i;
-                while(i<nums.length){
-                    if(nums[i] == target) end = i;
-                    i++;
-                }
-            } 
-            i++;
-        }
-
+        if(nums.length == 0 || nums[0] > target || nums[nums.length-1] < target) return new int[]{-1, -1};
+        int start = firstindex(nums,target);
+        int end = endindex(nums,target);
         return new int[]{start, end};
-    }
+        }
+        public static int firstindex(int[] nums, int target){
+        int low = 0, high = nums.length-1, index = -1;
+        while(low<=high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target){
+                index = mid;
+                high = mid-1;
+            }
+            else if(nums[mid] != target && nums[mid] < target) low = mid+1;
+            else high = mid-1;
+        }
+        return index;
+        }
+        public static int endindex(int[] nums, int target){
+        int low = 0, high = nums.length-1, index = -1;
+        while(low<=high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target){
+                index = mid;
+                low = mid+1;
+            }
+            else if(nums[mid] != target && nums[mid] < target) low = mid+1;
+            else high = mid-1;
+        }
+        return index;
+        }
+
 }
 
