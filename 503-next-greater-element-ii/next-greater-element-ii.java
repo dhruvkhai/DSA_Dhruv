ae3d08d3de4f463c29a016ebaed1a77e507f2d32
Dhruv Khaitan <129095159+dhruvkhai@users.noreply.github.com>
2025-08-05 18:07:09 +0530

    Time: 18 ms (27.56%) | Memory: 46.3 MB (62.07%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -4,11 +4,11 @@ class Solution {
         int[] ans = new int[n];
         Stack<Integer> st = new Stack<>();
         for(int i = 2*n ; i >= 0; i--){
-            while(!st.isEmpty() && nums[st.peek()] <= nums[i%n]){
+            while(!st.isEmpty() && st.peek() <= nums[i%n]){
                 st.pop();
             }
-            ans[i%n] = st.isEmpty() ? -1 : nums[st.peek()];
-            st.push(i%n);
+            ans[i%n] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums[i%n]);
         }
         return ans;
     }
