9e1b2ff0dcb6fbc3f733874f5cfa591fe91550f9
Dhruv Khaitan <129095159+dhruvkhai@users.noreply.github.com>
2025-08-14 23:24:26 +0530

    Time: 25 ms (37.84%) | Memory: 46.8 MB (27.80%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,104 +1,52 @@
-// class Solution {
-//     public int calculate(String s) {
-//         Stack<Integer> st = new Stack<>();
-//         char token = ' ';
-//         for(int i = 0; i < s.length(); i++){
-//             char c = s.charAt(i);
-//             if(Character.isDigit(c)){
-//                 int num = 0;
-//                 while (i < s.length() && Character.isDigit(s.charAt(i))) {
-//                     num = num * 10 + (s.charAt(i) - '0');
-//                     i++;
-//                 }
-//                 st.push(num);
-//                 token = s.charAt(i+1);
-//                 i--;
-//             }
-            
-            
-
-//             else if(c == '+' || c == '-' || c == '*' || c == '/'){
-//                 int a = st.pop();
-//                 int b = st.pop();
-//                 int res = 0;
-//                 switch(c){
-//                     case '+':
-//                         res = a+b;
-//                         break;
-                    
-//                     case '-' :
-//                         res = b-a;
-//                         break;
-                    
-//                     case '*' :
-//                         res = a*b;
-//                         break;
-                    
-//                     case '/' :
-//                         res = a/b;
-//                         break;
-//                 }
-//                 st.push(res);
-//             }
-//         }
-//         return st.peek();
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-import java.util.Stack;
-
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
-        int num = 0;
-        char lastOp = '+'; // last seen operator
+        Stack<Integer> st = new Stack<>();
+        char operator = '+';
+        int number = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < s.length() ; i++){
             char c = s.charAt(i);
-
-            // If digit, build the number
-            if (Character.isDigit(c)) {
-                num = num * 10 + (c - '0');
+            if(Character.isDigit(c)){
+                number = number * 10 + (c - '0');
             }
 
-            // If operator (or end of string), process previous number
-            if ((!Character.isDigit(c) && c != ' ') || i == s.length() - 1) {
-                switch (lastOp) {
+            if((!Character.isDigit(c) && c != ' ') || i == s.length() - 1){
+                switch(operator){
                     case '+':
-                        stack.push(num);
+                        st.push(number);
                         break;
+                    
                     case '-':
-                        stack.push(-num);
+                        st.push(-1 * number);
                         break;
+
                     case '*':
-                        stack.push(stack.pop() * num);
+                        st.push(number * st.pop());
                         break;
+
                     case '/':
-                        stack.push(stack.pop() / num);
+                        st.push(st.pop() / number);
                         break;
                 }
-                lastOp = c; // update the last seen operator
-                num = 0;    // reset number
+                operator = c;
+                number = 0;
             }
         }
-
-        // Sum up all numbers in stack
-        int result = 0;
-        while (!stack.isEmpty()) {
-            result += stack.pop();
+        int sum = 0; 
+        while(!st.isEmpty()){
+            sum = sum + st.pop();
         }
-
-        return result;
+        return sum;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
