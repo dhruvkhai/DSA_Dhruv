77cb6dc2aea6f9f366a6da92e063fca63619511c
Dhruv Khaitan <129095159+dhruvkhai@users.noreply.github.com>
2025-09-23 19:08:42 +0530

    Time: 1 ms (100.00%) | Memory: 45.5 MB (75.24%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -1,28 +1,24 @@
-import java.util.*;
-
 class Solution {
     public List<String> letterCasePermutation(String s) {
-        List<String> result = new ArrayList<>();
-        backtrack(s.toCharArray(), 0, result);
-        return result;
+        List<String> ans = new ArrayList<>();
+        backtrack(s.toCharArray(), ans, 0);
+        return ans;
     }
 
-    private void backtrack(char[] chars, int index, List<String> result) {
-        if (index == chars.length) {
-            result.add(new String(chars));
+    public void backtrack(char[] ch, List<String> ans, int index){
+        if(index == ch.length){
+            ans.add(new String(ch));
             return;
         }
 
-        if (Character.isLetter(chars[index])) {
-            // branch with lowercase
-            chars[index] = Character.toLowerCase(chars[index]);
-            backtrack(chars, index + 1, result);
+        if(Character.isDigit(ch[index])){
+            backtrack(ch, ans, index + 1);
+        }else{
+            ch[index] = Character.toUpperCase(ch[index]);
+            backtrack(ch, ans, index + 1);
 
-            // branch with uppercase
-            chars[index] = Character.toUpperCase(chars[index]);
-            backtrack(chars, index + 1, result);
-        } else {
-            backtrack(chars, index + 1, result);
+            ch[index] = Character.toLowerCase(ch[index]);
+            backtrack(ch, ans, index + 1); 
         }
     }
 }
