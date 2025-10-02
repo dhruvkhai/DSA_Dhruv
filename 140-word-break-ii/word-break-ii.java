c72534d82776c418911058940c897798d0ac5886
Dhruv Khaitan <129095159+dhruvkhai@users.noreply.github.com>
2025-10-02 20:37:41 +0530

    Time: 1 ms (92.77%) | Memory: 41.7 MB (79.48%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        List<String> ans = new ArrayList<>();
+        HashSet<String> wordDictMap = new HashSet<>(wordDict);
+        func(s, ans, 0, new ArrayList<>(), wordDictMap);
+        return ans; 
+    }
+    void func(String s, List<String> ans, int i, List<String> str, HashSet<String> wordDictMap){
+        if(i == s.length()){
+            ans.add(String.join(" ", str));
+            return;
+        }
+        for(int j = i; j <= s.length(); j++)
+        {
+            String word = s.substring(i, j);
+            if(wordDictMap.contains(word)){
+                str.add(word);
+                func(s, ans, j, str, wordDictMap);
+                str.remove(str.size()- 1);
+            }
+        }
+    }
+}
