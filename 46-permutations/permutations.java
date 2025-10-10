922909ad4f5b5357bf2157b59e9805d1585de452
Dhruv Khaitan <129095159+dhruvkhai@users.noreply.github.com>
2025-10-10 21:26:27 +0530

    Time: 1 ms (92.98%) | Memory: 44.3 MB (93.39%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,21 +1,31 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        func(nums, ans, new ArrayList<>());
-        return ans;
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), new boolean[nums.length], result);
+        return result;
     }
 
-    void func(int[] nums, List<List<Integer>> ans, List<Integer> list){
-        if(list.size() == nums.length){
-            ans.add(new ArrayList<>(list));
+    void backtrack(int[] nums, List<Integer> curr, boolean[] used, List<List<Integer>> result) {
+        // base case: if permutation complete
+        if (curr.size() == nums.length) {
+            result.add(new ArrayList<>(curr));
             return;
         }
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(list.contains(nums[i])) continue;
-            list.add(nums[i]);
-            func(nums, ans, list);
-            list.remove(list.size() - 1);
+
+        // explore all possibilities
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue; // skip already used elements
+
+            used[i] = true;              // choose
+            curr.add(nums[i]);           // add element
+
+            backtrack(nums, curr, used, result); // recurse
+
+            // backtrack
+            used[i] = false;             
+            curr.remove(curr.size() - 1);
         }
     }
 }
